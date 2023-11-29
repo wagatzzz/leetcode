a87585486d2b842e81fd716e6e7a2fadcfd93642
Wagatwe Maina <117917873+wagatzzz@users.noreply.github.com>
2023-11-30 00:04:51 +0300

    Time: 63 ms (9.70%), Space: 44.1 MB (10.16%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -9,8 +9,8 @@ var reduce = function(nums, fn, init) {
         return init;
     }else{
         var val = init;
-        for(i in nums){
-            val = fn(val, nums[i]);
+        for(i of nums){
+            val = fn(val, i);
         }
     }
     return val;
