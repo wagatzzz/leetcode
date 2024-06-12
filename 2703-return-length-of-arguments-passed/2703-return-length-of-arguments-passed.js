14440d04a7b6cc9f4d076d7988db3079c8506c9f
Wagatwe Maina <117917873+wagatzzz@users.noreply.github.com>
2024-06-12 23:21:32 +0300

    Time: 48 ms (74.93%), Space: 47.9 MB (98.14%) - LeetHub

--- 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -2,9 +2,10 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-var argumentsLength = function(...args) {
+
+
+function argumentsLength(...args){
     return args.length;
-	
 };
 
 /**
