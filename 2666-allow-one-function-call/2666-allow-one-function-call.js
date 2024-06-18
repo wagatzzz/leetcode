69621d2e9fc30f2e2e087662bff92f7064fd9dc9
Wagatwe Maina <117917873+wagatzzz@users.noreply.github.com>
2024-06-18 11:51:50 +0300

    Time: 56 ms (34.69%), Space: 48.3 MB (90.42%) - LeetHub

--- 2666-allow-one-function-call/2666-allow-one-function-call.js
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -2,25 +2,37 @@
  * @param {Function} fn
  * @return {Function}
  */
-var once = function(fn) {
+function once(fn) {
     let hasBeenCalled = false;
     let result;
 
-    return function(...args) {
+    return function() {
         if (!hasBeenCalled) {
+            result = fn(...arguments); // Spread operator (...) to pass arguments
             hasBeenCalled = true;
-            result = fn(...args);
             return result;
         } else {
             return undefined;
         }
     };
-};
+}
+
 
 /**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
+ Function Definition: We define a function once that takes another function fn as an argument.
+
+Initialization: Inside once, we declare two variables:
+
+hasBeenCalled: This boolean flag keeps track of whether fn has been called before.
+result: This variable stores the result of the first call to fn.
+Returned Function: We return an inner function that:
+
+Checks if fn has already been called (hasBeenCalled is false).
+If fn has not been called (hasBeenCalled is false):
+fn(...arguments) uses the spread operator ... to pass the arguments to the fn function.
+Sets hasBeenCalled to true to indicate that fn has now been called.
+Stores the result of fn in the result variable.
+Returns the result.
+If fn has already been called (hasBeenCalled is true):
+It simply returns undefined.
  */
